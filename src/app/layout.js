515209b446c86cd5869a1059949a6cfd5e221d2d
Ghettosaint <58518629+Ghettosaint@users.noreply.github.com>
2025-06-02 19:21:34 +0300

    Update layout.js
    Removing Google Calendar integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,10 @@
-'use client'
-
-import { SessionProvider } from 'next-auth/react'
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider>
-          {children}
-        </SessionProvider>
+        {children}
       </body>
     </html>
   )
