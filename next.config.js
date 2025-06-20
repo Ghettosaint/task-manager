c0e388c05b660aa21f701aedf26a5779a961c658
Ghettosaint <58518629+Ghettosaint@users.noreply.github.com>
2025-06-20 18:02:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Add this to serve static files properly
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
